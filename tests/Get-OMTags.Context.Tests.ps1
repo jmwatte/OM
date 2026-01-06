7a19bb9c86ffb7413250f303c37a30c87f73b2f4
jmwatte <jean.marie.winters@gmail.com>
2026-01-06 16:13:55 +0100

    test(get-omtags): skip TagLib failure test when TagLib is present in environment; avoid brittle Add-Type expectation

--- tests/Get-OMTags.Context.Tests.ps1
+++ tests/Get-OMTags.Context.Tests.ps1
@@ -3,6 +3,9 @@ Describe 'Get-OMTags TagLib failure uses Context' {
         Import-Module (Join-Path $PSScriptRoot '..\OM.psm1') -Force
         . (Join-Path $PSScriptRoot '..\Private\Utils\Show-Message.ps1')
 
+        # If TagLib is already present in the environment, skip this test because we can't force a missing Add-Type
+        if ([System.AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.FullName -like '*TagLib*' }) { Skip 'TagLib present in environment; skipping TagLib failure path' }
+
         # Mock Add-Type to throw to simulate TagLib load failure
         Mock -CommandName Add-Type -MockWith { throw "Simulated Add-Type failure" }
 
